d3848c0eb0e8202873a679033ea1deee51dea016
Trent Patterson <jtrent238@outlook.com>
2018-02-06 09:17:09 -0500

    Create init.lua

---
+++ MineTest/notenoughores/init.lua
@@ -0,0 +1,43 @@
+-- default (Minetest 0.4 mod)
+-- Most default stuff
+
+-- The API documentation in here was moved into doc/lua_api.txt
+
+WATER_ALPHA = 160
+WATER_VISC = 1
+LAVA_VISC = 7
+LIGHT_MAX = 14
+
+-- Definitions made by this mod that other mods can use too
+default = {}
+
+-- Load other files
+dofile(minetest.get_modpath("notenoughores").."/functions.lua")
+
+-- Set a noticeable inventory formspec for players
+minetest.register_on_joinplayer(function(player)
+	local cb = function(player)
+		minetest.chat_send_player(player:get_player_name(), "This is the [minimal] \"Minimal Development Test\" game. Use [minetest_game] for the real thing.")
+	end
+	minetest.after(2.0, cb, player)
+end)
+
+-- ITEMS
+minetest.register_craftitem('notenoughores:ItemCopperIngot', {
+    description = 'Copper Ingot',
+    inventory_image = 'ItemCopperIngot.png',
+    stack_max = 64,
+})
+
+
+
+-- CRAFTS
+
+--minetest.register_craft({
+--    output = 'epicproportions:pick_pat',
+--    recipe = {
+--      {'epicproportions:ingot_pat','epicproportions:ingot_pat','epicproportions:ingot_pat'},
+--      {'', 'group:stick', ''},
+--      {'', 'group:stick', ''},
+--    }
+--})
